faf3e9e099cfcaed5d3874e6f61c5b5ceaccbe3a
Tristan_Domche <alonedomche@gmail.com>
2026-07-18 05:54:10 -0400

    Refactoring de PersonsController, ajout d'un filtre pour alleger la methode

--- CRUDExample/Controllers/PersonsController.cs
+++ CRUDExample/Controllers/PersonsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CRUDExample.Filters.ActionFilters;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Rotativa.AspNetCore;
 using ServiceContracts;
@@ -25,6 +26,7 @@ namespace CRUDExample.Controllers
 
         [Route("[action]")]
         [Route("/")]
+        [TypeFilter(typeof(PersonsListActionFilter))]
         public async Task<IActionResult> Index(string searchBy, string? searchString,
             string sortBy = nameof(PersonResponse.PersonName),
             SortOrderOptions sortOrder = SortOrderOptions.ASC)
@@ -34,25 +36,15 @@ namespace CRUDExample.Controllers
             _logger.LogDebug($"searchBy: {searchBy}, searchString: {searchString}, sortBy: {sortBy}, sortOrder: {sortOrder}");
 
             //Search
-            ViewBag.SearchFields = new Dictionary<string, string>()
-            {
-                {nameof(PersonResponse.PersonName), "Person Name" },
-                 {nameof(PersonResponse.Email), "Email" },
-                 {nameof(PersonResponse.DateOfBirth), "Date of Birth" },
-                  {nameof(PersonResponse.Gender), "Gender" },
-                   {nameof(PersonResponse.CountryID), "Country" },
-                    {nameof(PersonResponse.Address), "Address" }
-            };
+            
 
             List<PersonResponse> persons = await _personsService.GetFilteredPersons(searchBy, searchString);
 
-            ViewBag.CurrentSearchBy = searchBy;
-            ViewBag.CurrentSearchString = searchString;
+           
 
             //Sort
             List<PersonResponse> sortedPersons = await _personsService.GetSortedPersons(persons, sortBy, sortOrder);
-            ViewBag.CurrentSortBy = sortBy;
-            ViewBag.CurrentSortOrder = sortOrder.ToString();
+            
 
             return View(sortedPersons);
         }
